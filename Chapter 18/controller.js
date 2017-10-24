7dd89d5834fd1c1455b2bd5e989515b85310d625
pedram_farzaneh <pfarzaneh007@gmial.com>
2017-10-24 12:25:14 +0330

    تغییر جدید در کنترلر

--- Chapter 18/controller.js
+++ Chapter 18/controller.js
@@ -1,7 +1,7 @@
 angular.module("exampleApp", [])
     .controller("defaultCtrl", function($scope) {
         $scope.init = function () {
-            $scope.data2 = [{ "name": "محمد رضا", "uid": "123" }, { "name": "مرتضی", "uid": "124" }, { "name": "محمد", "uid": "125" }
+            $scope.data2 = [{ "name": "محمد حسن", "uid": "123" }, { "name": "مرتضی", "uid": "124" }, { "name": "محمد", "uid": "125" }
             ];
             $("#tags").autocomplete({
                 source: function (request, response) {
@@ -17,7 +17,7 @@ angular.module("exampleApp", [])
         $scope.data = {
             cities: ["London", "New York", "Paris"],
             totalClick: 0
-        }
+        };
 
         $scope.$watch("data.totalClick", function(newVal) {
             console.log("Total click count: " + newVal)
@@ -29,7 +29,7 @@ angular.module("exampleApp", [])
             scope: {
                 counter: "=counter"
             },
-            link: function(scope, element, attrs) {
+            link: function(scope, element) {
                 element.on("click", function(event) {
                     console.log("Button click: " + event.target.innerText)
                     scope.$apply(function() {
