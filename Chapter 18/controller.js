ea354e0bb39cccb0d37bfa231c0af337adcf0185
pedram_farzaneh <pfarzaneh007@gmial.com>
2017-10-25 17:50:51 +0330

    route test

--- Chapter 18/controller.js
+++ Chapter 18/controller.js
@@ -39,3 +39,40 @@ angular.module("exampleApp", [])
             }
         }
     })
+
+
+var app2;
+app2 = angular.module("routeApp", ['ngRoute'])
+    .controller("routeCtrl", function ($scope, $location) {
+        $scope.citieList = ["Isfahan", "tehran"];
+        $scope.cityObj = {};
+        $scope.newCity = function () {
+            $scope.cityObj = {};
+            $scope.cityObj.name = "";
+            $location.path("edit");
+        }
+        $scope.editCity = function (item) {
+            $scope.cityObj = {};
+            $scope.cityObj.name = item;
+            $location.path("edit");
+        }
+        $scope.registerCity = function () {
+            var add = true;
+            for(var i = 0 ; i< $scope.citieList.length;i++){
+                if($scope.citieList[i] == $scope.cityObj.name){
+                    $scope.citieList[i] = $scop.cityObj.name;
+                    add=false;
+                }
+            }
+            if (add) {
+                $scope.citieList.push($scope.cityObj.name);
+            }
+
+            $location.path("");
+        }
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when("/", {templateUrl: "mainView.html"})
+            .when("/edit", {templateUrl: "editView.html"})
+    })
