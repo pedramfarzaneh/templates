876a9cb9bd59bab203ff9a9f7e9cc4f29309abed
pedram_farzaneh <pfarzaneh007@gmial.com>
2017-10-27 19:09:33 +0330

    سرویس و سرچ در آرایه از اشیا

--- Chapter 18/controller.js
+++ Chapter 18/controller.js
@@ -43,29 +43,44 @@ angular.module("exampleApp", [])
 
 var app2;
 app2 = angular.module("routeApp", ['ngRoute'])
-    .controller("routeCtrl", function ($scope, $location) {
-        $scope.citieList = ["Isfahan", "tehran"];
+    .service('myLogicFunctions',function () {
+        return{
+            FindByID: function (propertyName,SearchedID) {
+                return function (element) {
+                    return element[propertyName] == SearchedID;
+                }
+            }
+        }
+    })
+    .controller("routeCtrl", function ($scope, $location, myLogicFunctions) {
+        $scope.citieList = [{'id':1,'name':"Isfahan"}, {'id':2, 'name':'Tehran'}];
         $scope.cityObj = {};
+
         $scope.newCity = function () {
             $scope.cityObj = {};
             $scope.cityObj.name = "";
+            $scope.cityObj.id = "";
             $location.path("edit");
         }
         $scope.editCity = function (item) {
             $scope.cityObj = {};
-            $scope.cityObj.name = item;
+            $scope.cityObj = item;
             $location.path("edit");
         }
         $scope.registerCity = function () {
             var add = true;
-            for(var i = 0 ; i< $scope.citieList.length;i++){
-                if($scope.citieList[i] == $scope.cityObj.name){
-                    $scope.citieList[i] = $scop.cityObj.name;
-                    add=false;
-                }
+            // for(var i = 0 ; i< $scope.citieList.length;i++){
+            //     if($scope.citieList[i] == $scope.cityObj.name){
+            //         $scope.citieList[i] = $scop.cityObj.name;
+            //         add=false;
+            //     }
+            // }
+            var obj =$scope.citieList.find(myLogicFunctions.FindByID('id', $scope.cityObj.id));
+            if(typeof  obj != 'undefined'){
+                obj.name  = $scope.cityObj.name;
             }
-            if (add) {
-                $scope.citieList.push($scope.cityObj.name);
+            else {
+                $scope.citieList.push($scope.cityObj);
             }
 
             $location.path("");
